815e539ba75d1e127bb58cea81f2fa718b81cf9c
fengqi <lyf362345@gmail.com>
2024-11-26 22:30:28 +0800

    hdpi

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace WinLauncher {
     internal static class Program {
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main() {
+            SetProcessDPIAware();
             Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
